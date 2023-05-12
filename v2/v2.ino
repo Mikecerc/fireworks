87dab84b8e50fea39b074f76e892356a09b8ee64
mikecerc <mikecerc1@outlook.com>
2023-05-12 13:02:16 -0400

    developed const variables

--- v2/v2.ino
+++ v2/v2.ino
@@ -1,11 +1,60 @@
 #include <LiquidCrystal.h>
 #include <Keypad.h>
 
+const int Relay1 = 22;                 //relay pins
+const int Relay2 = 24;
+const int Relay3 = 26;
+const int Relay4 = 28;
+const int Relay5 = 30;
+const int Relay6 = 32;
+const int Relay7 = 34;
+const int Relay8 = 36;
+const int Relay9 = 23;
+const int Relay10 = 25;
+const int Relay11 = 27;
+const int Relay12 = 29;
+const int Relay13 = 31;
+const int Relay14 = 33;
+const int Relay15 = 35;
+const int Relay16 = 37;
+const int Relay17 = 38;
+const int Relay18 = 40;
+const int Relay19 = 42;
+const int Relay20 = 44;
+const int Relay21 = 46;
+const int Relay22 = 48;
+const int Relay23 = 50;
+const int Relay24 = 52;
+           
+const int contrelaypin = 9;    //pin for continuity relay/light
+const int contlightpin = 11;
+const int armlightpin = 10;     //pin for arm light
+const int disabledlightpin = 12;   //pin for diabled light/not armed or cont mode light
+
+const int LCDRSpin = 14;    //lcd pins
+const int LCDEpin = 15;
+const int LCDD4 = 16;
+const int LCDD5 = 17;
+const int LCDD6 = 18;
+const int LCDD7 = 19;
+
+const char KR0 = A7;        //keypad pins 
+const char KR1 = A6;       // KR= rows KC= columns
+const char KR2 = A5;
+const char KR3 = A4;
+const char KC0 = A3;
+const char KC1 = A2;
+const char KC2 = A1;
+const char KC3 = A0;
+
 const int relayFireTime = 2000;  //ms
 const int delayTime = 1000;      //ms
 const int totalRelays = 23;
 char keypressed;
+static bool armed = false;
+static bool continuity = false;
 void setup() {
+
 }
 
 void loop() {
@@ -70,8 +119,11 @@ public:
   static int pinNumbers[totalRelays];
   Relay(int pins[totalRelays]) {
     pinNumbers[totalRelays] = pins[totalRelays];
+    for (int i = 0; i < sizeof(pinNumbers); i++) {
+      digitalWrite(pinNumbers[i], HIGH);
+    };
   };
-  ignite(Display display, int relayNumber) {
+  void ignite(Display display, int relayNumber) {
     display.print("Ignition!");
     digitalWrite(pinNumbers[relayNumber], HIGH);
     delay(relayFireTime);
@@ -79,7 +131,7 @@ public:
     display.def();
     delay(delayTime);
   };
-  fire(ButtonPad buttonPad, Display display, int relayNumber) {
+  void fire(ButtonPad buttonPad, Display display, int relayNumber) {
     String message = "Fire";
     String number = String(relayNumber + 1);
     message += number;
